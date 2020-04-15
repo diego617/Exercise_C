0c9dfe4b2f7feb45082e2be0bbcfd0a97e253cc1
Diego617 <diegotacuricabezas617@gmail.com>
2020-04-15 17:37:24 -0500

    primera version

--- Exercise/Exercise/Program.cs
+++ Exercise/Exercise/Program.cs
@@ -7,7 +7,23 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+
+            string messge = "No tienes suficiente cantiedad";
+            int cuenta = 100;
+            int cantidad = 0;
+            Console.WriteLine($"Monto a Retirar: {cantidad}");
+            cantidad = int.Parse(Console.ReadLine());
+            
+            if(cantidad <= cuenta && cantidad >= 1) {
+                cuenta -=  cantidad;
+                Console.WriteLine($"Tu saldo: {cuenta}");
+            }
+            else {
+                Console.WriteLine(messge);
+            }
+            
             Console.WriteLine("Hello world");
+            Console.ReadKey();
         }
     }
 }
