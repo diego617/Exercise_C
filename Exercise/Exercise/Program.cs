ac3f476676cfa6f9facbe05a177c8d7feb1fe11c
Diego617 <diegotacuricabezas617@gmail.com>
2020-04-16 18:50:21 -0500

    version 2

--- Exercise/Exercise/Program.cs
+++ Exercise/Exercise/Program.cs
@@ -8,21 +8,28 @@ namespace Exercise {
     class Program {
         static void Main(string[] args) {
 
-            string messge = "No tienes suficiente cantiedad";
+            string messge = "No cuentas con la cantidad suficiente";
             int cuenta = 100;
-            int cantidad = 0;
-            Console.WriteLine($"Monto a Retirar: {cantidad}");
+            int cantidad;
+
             cantidad = int.Parse(Console.ReadLine());
-            
-            if(cantidad <= cuenta && cantidad >= 1) {
-                cuenta -=  cantidad;
-                Console.WriteLine($"Tu saldo: {cuenta}");
+            Console.WriteLine($"Monto a Retirar: {cantidad}");
+
+            if (cantidad <= cuenta && cantidad >= 1) {
+                if(cantidad % 20 == 0) {
+                    int res = cantidad / 20;
+                    Console.WriteLine($"resultado: {res}");
+                    cuenta -= cantidad;
+                    Console.WriteLine($"Tu saldo: {cuenta}");
+                }
+                else {
+                    Console.WriteLine("Solo Puedes retirar billestes de 20");
+                }
             }
             else {
                 Console.WriteLine(messge);
             }
-            
-           
+
             Console.ReadKey();
         }
     }
